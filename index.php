becd4ce3ac7cd09aaf3dd4effd10dd51cd009118
gaoqilin <334136724@qq.com>
2019-08-03 21:23:41 +0800

    init vimmarkdown

---
+++ index.php
@@ -0,0 +1,36 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="./github-markdown-css/github-markdown.css">
+<style>
+	.markdown-body {
+		box-sizing: border-box;
+		min-width: 200px;
+		max-width: 980px;
+		margin: 0 auto;
+		padding: 45px;
+	}
+
+	@media (max-width: 767px) {
+		.markdown-body {
+			padding: 15px;
+		}
+	}
+</style>
+<article class="markdown-body">
+<?php
+
+include './vendor/autoload.php';
+
+$markfile = "./markdown/mark.md";
+$text = file_get_contents($markfile);
+
+use Michelf\MarkdownExtra;
+$parser = new MarkdownExtra;
+$parser->fn_id_prefix = "post22-";
+$my_html = $parser->transform($text);
+echo $my_html;
+
+?>
+
+</article>
+
+
